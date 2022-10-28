fc7a9b5a72695f65435faaeb3da5afee0f90c60e
@reroes <reroes799@gmail.com>
2022-10-28 10:13:18 -0500

    ejercicios para taller v2

--- Ejercicio02/src/ejercicio02/Ejercicio02.java
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -19,9 +19,6 @@ public class Ejercicio02 {
         int var1 = 10;
         int var2 = 2;
         
-        double resultado = (Math.pow(var1, var2)) + var1;
-        
-        System.out.println(resultado);
                 
     }
     
--- Ejercicio03/src/ejercicio03/Ejercicio03.java
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -16,12 +16,7 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double resultado = (Math.pow(10, 2)) + Math.sqrt(16) + (20 / (1+11));
         
-        System.out.println(resultado);
-        
-        boolean variable11 = true;
-        boolean variable12 = false;
     }
     
 }
